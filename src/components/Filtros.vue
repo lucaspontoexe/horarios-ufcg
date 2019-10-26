e4b3f1539f4cde978c5078ed5119885698663ca1
lucaspontoexe <ltx16@hotmail.com>
2019-10-26 20:21:32 -0300

    Extraindo botão tag para um novo componente

--- src/components/Filtros.vue
+++ src/components/Filtros.vue
@@ -2,46 +2,24 @@
   <div class="filtros">
     <!-- todo: transformar botões em componentes -->
     <button class="toggle" :class="{ active: isActive }" @click="toggle">Optativas</button>
-    <button class="toggle" :class="{ active: isActive }" @click="toggle">1º P</button>
-    <button class="toggle" :class="{ active: isActive }" @click="toggle">2º P</button>
+    <tag />
   </div>
 </template>
 
-<style scoped>
+<style>
+
 .filtros {
   display: flex;
   flex-direction: row;
 }
 
-
-.toggle {
-  margin-right: 5px;
-
-  border-radius: 10px;
-  border-color: blueviolet;
-  background: #fff;
-  color: #444;
-  font-weight: bold;
-}
-
-.toggle.active {
-  background: blueviolet;
-  color: whitesmoke;
-}
 </style>
 
 <script>
+import tag from './Tag';
+
 export default {
   name: "filtros",
-  data() {
-    return {
-      isActive: true
-    };
-  },
-  methods: {
-    toggle() {
-      this.isActive = !this.isActive;
-    }
-  }
+  components: { tag },
 };
 </script>
---
+++ src/components/Tag.vue
@@ -0,0 +1,37 @@
+<template>
+  <button class="toggle" :class="{ active: isActive }" @click="toggle">Optativas</button>
+</template>
+
+<style>
+.toggle {
+  margin-right: 5px;
+
+  border-radius: 10px;
+  border-color: blueviolet;
+  background: #fff;
+  color: #444;
+  font-weight: bold;
+}
+
+.toggle.active {
+  background: blueviolet;
+  color: whitesmoke;
+}
+
+</style>
+
+<script>
+export default {
+  name: "tag",
+  data() {
+    return {
+      isActive: true
+    };
+  },
+  methods: {
+    toggle() {
+      this.isActive = !this.isActive;
+    }
+  }
+};
+</script>

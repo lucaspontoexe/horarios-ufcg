c7b05a80c1b5d54921941ed9116ccdd220c55bb5
lucaspontoexe <ltx16@hotmail.com>
2019-10-26 20:15:20 -0300

    Adicionando mais botões

--- src/components/Filtros.vue
+++ src/components/Filtros.vue
@@ -1,17 +1,33 @@
 <template>
   <div class="filtros">
-    <button class="toggle">Optativas</button>
+    <!-- todo: transformar botões em componentes -->
+    <button class="toggle" :class="{ active: isActive }" @click="toggle">Optativas</button>
+    <button class="toggle" :class="{ active: isActive }" @click="toggle">1º P</button>
+    <button class="toggle" :class="{ active: isActive }" @click="toggle">2º P</button>
   </div>
 </template>
 
 <style scoped>
-
-.toggle {
-    border-radius: 10px;
-    border-color: blueviolet;
-    background: #fff;
+.filtros {
+  display: flex;
+  flex-direction: row;
 }
 
+
+.toggle {
+  margin-right: 5px;
+
+  border-radius: 10px;
+  border-color: blueviolet;
+  background: #fff;
+  color: #444;
+  font-weight: bold;
+}
+
+.toggle.active {
+  background: blueviolet;
+  color: whitesmoke;
+}
 </style>
 
 <script>
